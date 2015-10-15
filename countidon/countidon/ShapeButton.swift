79feb4dcdfdcba344a4208e1dbb39bd43a46b6f4
Karsten Gresch <git-2015@by-doing.org>
2015-10-15 22:31:00 +0200

    Added Swift code

---
+++ countidon/countidon/ShapeButton.swift
@@ -0,0 +1,55 @@
+//
+//  ShapeButton.swift
+//  countidon
+//
+//  Created by Karsten Gresch on 15.10.15.
+//  Copyright © 2015 Closure One. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ShapeButton: UIView {
+  
+  // source: http://stackoverflow.com/questions/27432736/how-to-create-an-ibinspectable-of-type-enum?rq=1
+  enum CornerType: String {
+    case Rounded = "rounded"
+    case Circle = "circle"
+  }
+  
+  var cornerType = CornerType.Rounded
+  
+  
+  @available(*, unavailable, message="This property is reserved for Interface Builder. Use 'cornerType' instead.")
+  @IBInspectable var cornerTypeName: String? {
+    willSet {
+      if let newCornerType = CornerType(rawValue: newValue?.lowercaseString ?? "") {
+        cornerType = newCornerType
+      }
+    }
+  }
+  
+
+  @IBInspectable var radiusWidth: CGFloat = 30
+  @IBInspectable var radiusHeight: CGFloat = 30
+  
+  
+  override func layoutSubviews() {
+
+    let shapeLayer = CAShapeLayer()
+    
+    switch cornerType {
+    case .Rounded:
+      shapeLayer.path = UIBezierPath(roundedRect: bounds, byRoundingCorners: [UIRectCorner.TopLeft,  UIRectCorner.TopRight, UIRectCorner.BottomLeft, UIRectCorner.BottomRight], cornerRadii: CGSize(width: 30, height: 30)).CGPath
+    case .Circle:
+      shapeLayer.path = UIBezierPath(ovalInRect: self.bounds).CGPath
+    }
+    layer.mask = shapeLayer
+   
+    
+  }
+  
+
+
+
+}
---
+++ countidon/countidon/StartScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StartScreenViewController.swift
+//  countidon
+//
+//  Created by Karsten Gresch on 15.10.15.
+//  Copyright © 2015 Closure One. All rights reserved.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+  @IBOutlet weak var upperStartScreenBackgroundView: BackgroundView!
+  @IBOutlet weak var lowerStartScreenBackgroundView: BackgroundView!
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
