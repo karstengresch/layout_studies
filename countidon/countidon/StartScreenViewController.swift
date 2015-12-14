507d72e5dc57d406acb578ab2a25e4b93a7f22d9
Karsten Gresch <git-2015@by-doing.org>
2015-12-14 23:09:50 +0100

    Unclear if implementing w/ Prototype cells or programmatically is better.

--- countidon/countidon/StartScreenViewController.swift
+++ countidon/countidon/StartScreenViewController.swift
@@ -46,12 +46,18 @@ class StartScreenViewController: UITableViewController {
   
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     // #warning Incomplete implementation, return the number of sections
-    return 2
+    return 1
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     // #warning Incomplete implementation, return the number of rows
-    return 1
+    return 2
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("StartScreenCell", forIndexPath: indexPath)
+    
+    return cell
   }
   
 
