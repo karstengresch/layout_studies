df7a28f594f74968afae1f6b1057ee266da32d10
Karsten Gresch <git-2015@by-doing.org>
2015-12-14 22:57:05 +0100

    Argh, crashed

--- countidon/countidon/StartScreenViewController.swift
+++ countidon/countidon/StartScreenViewController.swift
@@ -35,5 +35,24 @@ class StartScreenViewController: UITableViewController {
     self.navigationController?.navigationBar.hidden = true
 
   }
+  
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 2
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return 1
+  }
+  
 
 }
