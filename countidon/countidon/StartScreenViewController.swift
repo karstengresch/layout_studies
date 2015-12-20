853403f6e960f4a6c6153287f248cdeebc1a8d85
Karsten Gresch <git-2015@by-doing.org>
2015-12-20 23:23:13 +0100

    Tried overriding the prototype cell's background view (color) - but it didn't work. Overriding the custom button's color neither.

--- countidon/countidon/StartScreenViewController.swift
+++ countidon/countidon/StartScreenViewController.swift
@@ -9,29 +9,12 @@
 import UIKit
 
 class StartScreenViewController: UITableViewController, CounterViewControllerDelegate, SettingsViewControllerDelegate {
-  @IBOutlet weak var upperStartScreenBackgroundView: BackgroundView?
-  @IBOutlet weak var lowerStartScreenBackgroundView: BackgroundView?
   @IBOutlet weak var startScreenNavigationItem: UINavigationItem?
-  
-  @IBOutlet weak var upperShapeButton: ShapeButton?
-  @IBOutlet weak var lowerShapeButton: ShapeButton?
   
   let counterViewController = CounterViewController()
   let settingsViewController = SettingsViewController()
 
-  @IBAction func lowerStartScreenButtonPressed(sender: ShapeButton) {
-    print("Lower start screen button pressed!")
-    // self.navigationController?.pushViewController(settingsViewController, animated: true)
-    
-  }
-  
-  @IBAction func upperStartScreenButtonPressed(sender: ShapeButton) {
-        print("Upper start screen button pressed!")
-    // self.navigationController?.pushViewController(counterViewController, animated: true)
-  }
-  
-  
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     self.navigationController?.navigationBar.hidden = true
@@ -40,14 +23,10 @@ class StartScreenViewController: UITableViewController, CounterViewControllerDel
   }
   
   func setupTableViewDimensions() {
-    // tableView.rowHeight = UITableViewAutomaticDimension
     let screenRect: CGRect = UIScreen.mainScreen().bounds
     let screenHeight: CGFloat = screenRect.size.height
     print("Screen height: \(screenHeight)" )
     tableView.rowHeight = screenHeight / 2
-//     tableView.frame.height = super.screem
-    
-//     tableView.estimatedRowHeight = tableView.
   }
   
 
@@ -89,20 +68,19 @@ class StartScreenViewController: UITableViewController, CounterViewControllerDel
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("StartScreenCell", forIndexPath: indexPath)
     
-    let background = cell.viewWithTag(650) as! BackgroundView
     let circle = cell.viewWithTag(755) as! ShapeButton
     
     switch indexPath {
       case 0:
       print("cellForRowAtIndexPath selected: \(indexPath.row)" )
-      background.backgroundColor = UIColor(hue:0.568, saturation:0.673, brightness:0.734, alpha:0.77)
+      cell.backgroundView = cellBackground(UIColor(hue:0.568, saturation:0.673, brightness:0.734, alpha:0.77))
       circle.backgroundColor =  UIColor(hue:0.574, saturation:0.744, brightness:0.486, alpha:1)
       break
       
     case 1:
       print("cellForRowAtIndexPath selected: \(indexPath.row)" )
-      background.backgroundColor = UIColor(hue:0.574, saturation:0.744, brightness:0.486, alpha:1)
-       circle.backgroundColor =  UIColor(hue:0.568, saturation:0.673, brightness:0.734, alpha:0.77)
+      cell.backgroundView = cellBackground(UIColor(hue:0.574, saturation:0.744, brightness:0.486, alpha:1))
+      circle.backgroundColor =  UIColor(hue:0.568, saturation:0.673, brightness:0.734, alpha:0.77)
       break
     
     default:
@@ -112,6 +90,21 @@ class StartScreenViewController: UITableViewController, CounterViewControllerDel
     
     return cell
   }
+  
+  func cellBackground(color: UIColor?) -> UIView {
+    let backgroundView: UIView = UIView()
+    if let customColor = backgroundView.backgroundColor {
+      backgroundView.backgroundColor = customColor
+    }
+    
+    backgroundView.layer.masksToBounds = true
+    
+    return backgroundView
+    
+  }
+  
+  
+  
   
   // MARK: TableView delegates
   override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
@@ -128,18 +121,6 @@ class StartScreenViewController: UITableViewController, CounterViewControllerDel
       break // ignore
     }
   }
-  
-//  override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-//    var rowHeight: CGFloat = 0.0
-//    
-//    let screenRect: CGRect = UIScreen.mainScreen().bounds
-//    let screenHeight: CGFloat = screenRect.size.height
-//    print("Screen height: \(screenHeight) for indexPath.row: \(indexPath.row)" )
-//    rowHeight = screenHeight / 2
-//    
-//    return rowHeight
-//
-//  }
   
 
 }
