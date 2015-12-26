3eb910d02f18553228d4c39bb1baca5e130f5a15
Karsten Gresch <git-2015@by-doing.org>
2015-12-26 12:52:58 +0100

    Implemented basic group type

--- countidon/countidon/CountidonGroup.swift
+++ countidon/countidon/CountidonGroup.swift
@@ -6,9 +6,24 @@
 //  Copyright © 2015 Closure One. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+import Foundation
 
 class CountidonGroup: NSObject, NSCoding {
+  var name = ""
   
+  required init?(coder aDecoder: NSCoder) {
+    name = aDecoder.decodeObjectForKey("CountidonGroupName") as! String
+    super.init()
+  }
+  
+  func encodeWithCoder(aCoder: NSCoder) {
+    aCoder.encodeObject(name, forKey: "CountidonGroupName")
+  }
+  
+  init(name: String) {
+    self.name = name
+    super.init()
+  }
 
 }
