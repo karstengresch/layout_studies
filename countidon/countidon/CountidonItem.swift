109bcd4146d5e5c14db2bf04f0fea8957d7a4cb8
Karsten Gresch <git-2015@by-doing.org>
2015-12-26 12:51:20 +0100

    Implemented basic counter type

--- countidon/countidon/CountidonItem.swift
+++ countidon/countidon/CountidonItem.swift
@@ -6,13 +6,30 @@
 //  Copyright © 2015 Closure One. All rights reserved.
 //
 
-import Cocoa
-
+import UIKit
+import Foundation
+/**
+ The individual counter item.
+*/
 class CountidonItem: NSObject, NSCoding {
+  let created: NSDate
+  var name = ""
   
+  required init?(coder aDecoder: NSCoder) {
+    created = aDecoder.decodeObjectForKey("CountidonItemCreated") as! NSDate
+    name = aDecoder.decodeObjectForKey("CountidonItemName") as! String
+    super.init()
+  }
   
+  func encodeWithCoder(aCoder: NSCoder) {
+    aCoder.encodeObject(created, forKey: "CountidonItemCreated")
+    aCoder.encodeObject(name, forKey: "CountidonItemName")
+  }
   
-  
-  
+  init(name: String) {
+    self.created = NSDate()
+    self.name = name
+    super.init()
+  }
 
 }
