f2fe4f038f49bbe4805943cc890977170b2267c7
Karsten Gresch <git-2015@by-doing.org>
2016-01-09 19:05:50 +0100

    Added the main data model file

---
+++ countidon/countidon/CountidonDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  CountidonDataModel.swift
+//  countidon
+//
+//  Created by Karsten Gresch on 09.01.16.
+//  Copyright © 2016 Closure One. All rights reserved.
+//
+import UIKit
+import Foundation
