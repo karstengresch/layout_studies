55f0b6db3c5c58db25fdfa641ce5ef8145996142
Karsten Gresch <git-2015@by-doing.org>
2016-01-09 19:10:23 +0100

    Added persistence for top hierarchy class - settings 2b impl w/ NSUserDefaults

--- countidon/countidon/CountidonDataModel.swift
+++ countidon/countidon/CountidonDataModel.swift
@@ -7,3 +7,45 @@
 //
 import UIKit
 import Foundation
+
+class CountidonDataModel {
+  
+  var countidonGroups = [CountidonGroup]()
+  
+  init() {
+    print("Data file path is \(dataFilePath())")
+    loadCountidonGroups()
+  }
+  
+  // MARK: File related
+  func documentsDirectory() -> String {
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+    return paths[0]
+  }
+  
+  func dataFilePath() -> String {
+    return (documentsDirectory() as NSString).stringByAppendingPathComponent("CountidonGroups.plist")
+  }
+  
+  func saveCountidonGroups() {
+    let data = NSMutableData()
+    let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+    archiver.encodeObject(countidonGroups, forKey: "CountidonGroups")
+    archiver.finishEncoding()
+    data.writeToFile(dataFilePath(), atomically: true)
+  }
+  
+  func loadCountidonGroups() {
+    let path = dataFilePath()
+    if NSFileManager.defaultManager().fileExistsAtPath(path) {
+      if let data = NSData(contentsOfFile: path) {
+        let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+        countidonGroups = unarchiver.decodeObjectForKey("CountidonGroups") as! [CountidonGroup]
+        unarchiver.finishDecoding()
+      }
+    }
+  }
+
+  
+  
+}
