b76243bc48d1d29c40481ef7d5fadd2c05f202b7
Karsten Gresch <git-2015@by-doing.org>
2016-01-21 21:51:37 +0100

    Added new VC file for individual counter settings

---
+++ countidon/countidon/IndividualCounterSettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IndividualCounterSettingsViewController.swift
+//  countidon
+//
+//  Created by Karsten Gresch on 21.01.16.
+//  Copyright © 2016 Closure One. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class IndividualCounterSettingsViewController: UITableViewController {
+  
+  
+  
+
+}
