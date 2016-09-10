1f33807ad4103cbbdd9a1c2ddaa18f7c7842c371
Karsten Gresch <git-2015@by-doing.org>
2016-09-10 22:15:12 +0200

    Changed the TV setup

--- countidon/countidon/SettingsViewController.swift
+++ countidon/countidon/SettingsViewController.swift
@@ -51,7 +51,7 @@ class SettingsViewController: UITableViewController {
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     // #warning Incomplete implementation, return the number of rows
-    return 1
+    return 0
   }
   
   /*
