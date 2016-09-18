b8de0c75f9e0b2ac3898165aa0fdae1aaaf8986e
Karsten Gresch <git-2015@by-doing.org>
2016-09-18 20:58:46 +0200

    Migrating to Swift 3

--- countidon/countidon/StartScreenTableViewCell.swift
+++ countidon/countidon/StartScreenTableViewCell.swift
@@ -16,12 +16,8 @@ class StartScreenTableViewCell: UITableViewCell {
     super.awakeFromNib()
   }
   
-  override func setSelected(selected: Bool, animated: Bool) {
+  override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
   }
   
-
-
-  
-
 }
