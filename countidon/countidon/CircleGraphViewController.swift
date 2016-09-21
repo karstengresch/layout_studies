afdc7e60b038cb5607c7e6a986e4047b0e653f8c
Karsten Gresch <git-2015@by-doing.org>
2016-09-21 22:26:55 +0200

    Segue not working

--- countidon/countidon/CircleGraphViewController.swift
+++ countidon/countidon/CircleGraphViewController.swift
@@ -37,6 +37,7 @@ class CircleGraphViewController: UIViewController, IndividualCounterSettingsView
     }
   
   // MARK: IB related
+  // Leads to crash.
   @IBAction func cancel() {
     dismiss(animated: true, completion: nil)
   }
