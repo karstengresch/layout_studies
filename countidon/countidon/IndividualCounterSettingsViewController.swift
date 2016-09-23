a47951bcb05de3fc08b066f750329ea07b65fa12
Karsten Gresch <git-2015@by-doing.org>
2016-09-23 20:59:42 +0200

    Changed TVC

--- countidon/countidon/IndividualCounterSettingsViewController.swift
+++ countidon/countidon/IndividualCounterSettingsViewController.swift
@@ -48,12 +48,12 @@ class IndividualCounterSettingsViewController: UITableViewController {
   
   override func numberOfSections(in tableView: UITableView) -> Int {
     // #warning Incomplete implementation, return the number of sections
-    return 1
+    return 0
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     // #warning Incomplete implementation, return the number of rows
-    return 1
+    return 0
   }
   
 
