8b589df0905e0ab889f14f018a870b780fa06584
Karsten Gresch <git-2015@by-doing.org>
2016-10-04 21:56:12 +0200

    Reimplementing the settings views

--- countidon/countidon/SettingsViewController.swift
+++ countidon/countidon/SettingsViewController.swift
@@ -51,7 +51,7 @@ class SettingsViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     // #warning Incomplete implementation, return the number of rows
-    return 0
+    return 1
   }
   
   /*
