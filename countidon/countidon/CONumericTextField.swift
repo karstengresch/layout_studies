bb597c9a9d5f67cc281fc8b787b9391707924c6c
Karsten Gresch <git-2015@by-doing.org>
2016-10-05 21:42:15 +0200

    Individual text field for numbers

---
+++ countidon/countidon/CONumericTextField.swift
@@ -0,0 +1,43 @@
+//
+//  CONumericTextField.swift
+//  countidon
+//
+//  Created by Karsten Gresch on 05.10.16.
+//  Copyright © 2016 Closure One. All rights reserved.
+import UIKit
+
+private var numberTextMaxLength = [UITextField: Int]()
+
+extension UITextField {
+  
+  @IBInspectable var maxNumberLength: Int {
+    get {
+      guard let length = numberTextMaxLength[self] else {
+        return Int.max
+      }
+      return length
+    }
+    set {
+      numberTextMaxLength[self] = newValue
+      addTarget(
+        self,
+        action: #selector(limitLengthAction),
+        for: UIControlEvents.editingChanged
+      )
+    }
+  }
+  
+  func limitLengthAction(textField: UITextField) {
+    guard let numberText = textField.text,
+      numberText.characters.count > maxNumberLength
+      else {
+        return
+    }
+    
+    let selection = selectedTextRange
+    let maxIndex = numberText.index(numberText.startIndex, offsetBy: maxNumberLength)
+    text = numberText.substring(to: maxIndex)
+    selectedTextRange = selection
+  }
+  
+}
