f68b88eed98dbea28ac36749b112dbb882d51d76
Karsten Gresch <git-2015@by-doing.org>
2016-10-08 17:19:05 +0200

    Working on the numeric text field

--- countidon/countidon/CONumericTextField.swift
+++ countidon/countidon/CONumericTextField.swift
@@ -6,40 +6,42 @@
 //  Copyright © 2016 Closure One. All rights reserved.
 import UIKit
 
-private var numberTextMaxLength = [UITextField: Int]()
-// TODO only allow numbers. wondering if this'll work w/o a UITextField delegate
-private var allowedChars = "0123456789"
 
-extension UITextField {
+class NumericTextField: UITextField, UITextFieldDelegate {
   
-  @IBInspectable var maxNumberLength: Int {
-    get {
-      guard let length = numberTextMaxLength[self] else {
-        return Int.max
+  private var numberTextMaxLength = [UITextField: Int]()
+  // TODO only allow numbers. wondering if this'll work w/o a UITextField delegate
+  private var allowedChars = "0123456789"
+  
+    
+    @IBInspectable var maxNumberLength: Int {
+      get {
+        guard let length = numberTextMaxLength[self] else {
+          return Int.max
+        }
+        return length
       }
-      return length
+      set {
+        numberTextMaxLength[self] = newValue
+        addTarget(
+          self,
+          action: #selector(limitLengthAction),
+          for: UIControlEvents.editingChanged
+        )
+      }
     }
-    set {
-      numberTextMaxLength[self] = newValue
-      addTarget(
-        self,
-        action: #selector(limitLengthAction),
-        for: UIControlEvents.editingChanged
-      )
-    }
-  }
-  // TODO
-  func limitLengthAction(textField: UITextField) {
-    guard let numberText = textField.text,
-      numberText.characters.count > maxNumberLength
-      else {
-        return
+    // TODO
+    func limitLengthAction(textField: UITextField) {
+      guard let numberText = textField.text,
+        numberText.characters.count > maxNumberLength
+        else {
+          return
+      }
+      
+      let selection = selectedTextRange
+      let maxIndex = numberText.index(numberText.startIndex, offsetBy: maxNumberLength)
+      text = numberText.substring(to: maxIndex)
+      selectedTextRange = selection
     }
     
-    let selection = selectedTextRange
-    let maxIndex = numberText.index(numberText.startIndex, offsetBy: maxNumberLength)
-    text = numberText.substring(to: maxIndex)
-    selectedTextRange = selection
   }
-  
-}
