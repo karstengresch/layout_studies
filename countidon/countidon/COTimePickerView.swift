89ce3182582f7700c19fad4183825ad92a9d35dc
Karsten Gresch <git-2015@by-doing.org>
2016-10-13 22:23:21 +0200

    Further working on global settings

--- countidon/countidon/COTimePickerView.swift
+++ countidon/countidon/COTimePickerView.swift
@@ -3,19 +3,92 @@
 //  countidon
 //
 //  Created by Karsten Gresch on 13.10.16.
+//  Derived from StackOverflow http://stackoverflow.com/questions/10999575/uipickerview-that-looks-like-uidatepicker-but-with-seconds ...
+//  ... from the kind fella Mikael, http://stackoverflow.com/users/2015572/mikael
 //  Copyright © 2016 Closure One. All rights reserved.
 //
 
 import UIKit
 
-class COTimePickerView: UIPickerView {
+class COTimePickerView: UIPickerView, UIPickerViewDataSource, UIPickerViewDelegate {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var hour:Int = 0
+    var minute:Int = 0
+    
+    required internal init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+      self.setup()
     }
-    */
-
+  
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      self.setup()
+    }
+    
+    func setup(){
+      self.delegate = self
+      self.dataSource = self
+      
+      /*let height = CGFloat(20)
+       let offsetX = self.frame.size.width / 3
+       let offsetY = self.frame.size.height/2 - height/2
+       let marginX = CGFloat(42)
+       let width = offsetX - marginX
+       
+       let hourLabel = UILabel(frame: CGRectMake(marginX, offsetY, width, height))
+       hourLabel.text = "hour"
+       self.addSubview(hourLabel)
+       
+       let minsLabel = UILabel(frame: CGRectMake(marginX + offsetX, offsetY, width, height))
+       minsLabel.text = "min"
+       self.addSubview(minsLabel)*/
+    }
+    
+    func getDate() -> NSDate{
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "HH:mm"
+      let date = dateFormatter.date(from: String(format: "%02d", self.hour) + ":" + String(format: "%02d", self.minute))
+      return date! as NSDate
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+      return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+      switch component {
+      case 0:
+        self.hour = row
+      case 1:
+        self.minute = row
+      default:
+        print("No component with number \(component)")
+      }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+      if component == 0 {
+        return 24
+      }
+      
+      return 60
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+      return 30
+    }
+    
+  // looks dangerous now
+  func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+      if (view != nil) {
+        (view as! UILabel).text = String(format:"%02lu", row)
+        return view!
+      }
+      let columnView = UILabel(frame: CGRect(x: 35, y: 0, width: self.frame.size.width/3 - 35, height: 30))
+      columnView.text = String(format:"%02lu", row)
+      columnView.textAlignment = NSTextAlignment.center
+      
+      return columnView
+    }
+    
 }
